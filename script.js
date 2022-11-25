ef3030ecde6e69577777b27a2130fd0606b1951d
Himanshu Chhatwal <32031706+himanshu1221@users.noreply.github.com>
2022-11-25 12:04:04 +0000

    HappierMinds

---
+++ script.js
@@ -0,0 +1,74 @@
+
+
+// logic for top button 
+
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
+
+// logic for piter slideshow
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+// logic for scrolling smoothly
+
+document.querySelectorAll('a[href^=""#]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault()
+    Document.querySelectorAll(this.getAttribute("href").scrollIntoView({
+      behaviour: "smooth"
+    }))
+  })
+})
+
+
+document.read(function() {
+  (".sidebar-form .call-action").click(function() {
+    this.parents(".sidebar-form").toggleClass("show")
+  })
+})
+
+// logic for signin form 
+
+const loginText = document.querySelector(".title-text .login");
+const loginForm = document.querySelector("form.login");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector("form .signup-link a");
+signupBtn.onclick = (() => {
+  loginForm.style.marginLeft = "-50%";
+  loginText.style.marginLeft = "-50%";
+});
+loginBtn.onclick = (() => {
+  loginForm.style.marginLeft = "0%";
+  loginText.style.marginLeft = "0%";
+});
+signupLink.onclick = (() => {
+  signupBtn.click();
+  return false;
+});
